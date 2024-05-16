aa69411177a4291af144592486db887605497bbe
Vansh Tah <dev.vanshtah@gmail.com>
2024-05-17 04:30:11 +0530

    first commit

---
+++ src/config/subgraph.ts
@@ -0,0 +1,29 @@
+import { ARBITRUM, PEGASUS, PHOENIX } from "./chains";
+
+export const SUBGRAPH_URLS = {
+  [ARBITRUM]: {
+    stats: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-stats",
+    referrals: "https://api.thegraph.com/subgraphs/name/gmx-io/gmx-arbitrum-referrals",
+    nissohVault: "https://api.thegraph.com/subgraphs/name/nissoh/gmx-vault",
+  },
+
+  [PEGASUS]: {
+    stats: "https://graph.pegasus.lightlink.io/query/subgraphs/name/amped-finance/stats",
+    referrals: "https://graph.pegasus.lightlink.io/query/subgraphs/name/amped-finance/referrals",
+    trades:"https://graph.pegasus.lightlink.io/query/subgraphs/name/amped-finance/trades",  
+    raw:"https://graph.pegasus.lightlink.io/query/subgraphs/name/amped-finance/raw2",  
+    price: "https://graph.pegasus.lightlink.io/query/subgraphs/name/amped-finance/prices"
+  },
+  
+  [PHOENIX]: {
+    stats: "https://info.amped.finance/subgraphs/name/amped/stats",
+    referrals: "https://info.amped.finance/subgraphs/name/amped/referrals",
+    trades:"https://info.amped.finance/subgraphs/name/amped/trades",  
+    raw:"https://info.amped.finance/subgraphs/name/amped/raw",  
+    price: "http://info3.amped.finance/subgraphs/name/amped/prices"
+  },
+  
+  common: {
+    chainLink: "http://info3.amped.finance/subgraphs/name/amped/prices"
+  },
+};
---
+++ src/lib/subgraph/clients.ts
@@ -0,0 +1,65 @@
+import { createClient } from "./utils";
+import { SUBGRAPH_URLS } from "config/subgraph";
+import { ARBITRUM, PEGASUS, PHOENIX } from "config/chains";
+
+export const chainlinkClient = createClient(SUBGRAPH_URLS.common.chainLink);
+
+export const arbitrumGraphClient = createClient(SUBGRAPH_URLS[ARBITRUM].stats);
+export const arbitrumReferralsGraphClient = createClient(SUBGRAPH_URLS[ARBITRUM].referrals);
+export const nissohGraphClient = createClient(SUBGRAPH_URLS[ARBITRUM].nissohVault);
+
+export const pegasusGraphClient = createClient(SUBGRAPH_URLS[PEGASUS].stats);
+export const pegasusReferralsGraphClient = createClient(SUBGRAPH_URLS[PEGASUS].referrals);
+export const pegasusGraphClientForTrades = createClient(SUBGRAPH_URLS[PEGASUS].trades);
+export const pegasusGraphClientForRaw = createClient(SUBGRAPH_URLS[PEGASUS].raw);
+
+export const phoenixGraphClient = createClient(SUBGRAPH_URLS[PHOENIX].stats);
+export const phoenixReferralsGraphClient = createClient(SUBGRAPH_URLS[PHOENIX].referrals);
+export const phoenixGraphClientForTrades = createClient(SUBGRAPH_URLS[PHOENIX].trades);
+export const phoenixGraphClientForRaw = createClient(SUBGRAPH_URLS[PHOENIX].raw);
+
+export function getAmpGraphClient(chainId: number) {
+  if (chainId === ARBITRUM) {
+    return arbitrumGraphClient;
+  } else if (chainId === PEGASUS) {
+    return pegasusGraphClient;
+  } else if (chainId === PHOENIX) {
+    return phoenixGraphClient;
+  }
+
+  throw new Error(`Unsupported chain ${chainId}`);
+}
+
+export function getTradesGraphClient(chainId: number) {
+  if (chainId === ARBITRUM) {
+    return nissohGraphClient;
+  } else if (chainId === PEGASUS) {
+    return pegasusGraphClientForTrades;
+  } else if (chainId === PHOENIX) {
+    return phoenixGraphClientForTrades;
+  }
+
+  throw new Error(`Unsupported chain ${chainId}`);
+}
+
+export function getRawGraphClient(chainId: number) {
+  if (chainId === PEGASUS) {
+    return pegasusGraphClientForRaw;
+  } else if (chainId === PHOENIX) {
+    return phoenixGraphClientForRaw;
+  }
+
+  throw new Error(`Unsupported chain ${chainId}`);
+}
+
+export function getAmpGraphClientByNewCreate(chainId: number) {
+  if (chainId === ARBITRUM) {
+    return createClient(SUBGRAPH_URLS[ARBITRUM].stats);
+  } else if (chainId === PEGASUS) {
+    return createClient(SUBGRAPH_URLS[PEGASUS].stats);
+  } else if (chainId === PHOENIX) {
+    return createClient(SUBGRAPH_URLS[PHOENIX].stats);
+  }
+
+  throw new Error(`Unsupported chain ${chainId}`);
+}
