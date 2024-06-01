a3109f7828b0ca98e40c00180b2bd14383c628ff
dancube <denright@gmail.com>
2024-06-01 14:41:17 +1000

    remove max lev check

--- src/components/Exchange/SwapBox.js
+++ src/components/Exchange/SwapBox.js
@@ -880,8 +880,8 @@ export default function SwapBox(props) {
       return [t`Min leverage: 1.1x`];
     }
 
-    if (leverage && leverage.gt(8 * BASIS_POINTS_DIVISOR)) {
-      return [t`Max leverage: 8x`];
+    if (leverage && leverage.gt(50 * BASIS_POINTS_DIVISOR)) {
+      return [t`Max leverage: 50x`];
     }
 
     if (!isMarketOrder && entryMarkPrice && triggerPriceUsd && !savedShouldDisableValidationForTesting) {
