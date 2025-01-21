862c31200a9c15770aefce6d563b5b6524969027
dancube <denright@gmail.com>
2025-01-21 21:46:06 +1100

    update leaderboard subgraph

--- src/config/subgraph.ts
+++ src/config/subgraph.ts
@@ -25,7 +25,7 @@ export const SUBGRAPH_URLS = {
     raw:"https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/trades",  
     price: "https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/trades",
     ordernew: "https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/orders",
-    points: "https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/tradespointsnew2"
+    points: "https://graph.phoenix.lightlink.io/query/subgraphs/name/amped-finance/leaderboard"
   },
 
   [BSCTESTNET]: {
