98a34cad36b8248f0000846dffe8250e80e974ce
dancube <denright@gmail.com>
2025-02-17 14:12:02 +1100

    add header

---
+++ src/components/Header/UpdateBanner.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './UpdateBanner.css';
+
+export function UpdateBanner() {
+  return (
+    <div className="update-banner">
+      <div className="update-banner-content">
+        Sonic contracts have been re-deployed, see the announcement for more details &nbsp;
+        <a 
+          href="https://twitter.com/AmpedFinance/status/1891308056752869753" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          className="update-banner-link"
+        >
+          Read announcement
+        </a>
+      </div>
+    </div>
+  );
+} 
